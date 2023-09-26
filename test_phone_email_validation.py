2fe3072c0c1292bba920a1dd7962ab2c2ed7a6d0
Kartik-Nair <kartiknair1999@gmail.com>
2023-09-26 13:10:59 -0400

    Phone number and email validation done

---
+++ test_phone_email_validation.py
@@ -0,0 +1,23 @@
+import unittest
+
+# This import is failing with error: ImportError: attempted relative import with no known parent package
+import phonebook
+
+class TestPhoneNumberAndEmailAddressValidation(unittest.TestCase):
+
+    def test_valid_phone_number(self):
+        self.assertTrue(phonebook.Phonebook().validate_phone_number("(123) 456-7890"), "Should be a valid phone number")
+
+    def test_invalid_phone_number(self):
+        self.assertFalse(phonebook.Phonebook().validate_phone_number("1234567890"), "Should be an invalid phone number")
+
+    def test_valid_email_address(self):
+        self.assertTrue(phonebook.Phonebook().validate_email_address("knair044@uottawa.ca"), "Should be a valid email address")
+
+    def test_invalid_email_address(self):
+        self.assertFalse(phonebook.Phonebook().validate_email_address("knair044uottawaca"), "Should be an invalid email address")
+       
+if __name__ == "__main__":
+    unittest.main()
+
+    
